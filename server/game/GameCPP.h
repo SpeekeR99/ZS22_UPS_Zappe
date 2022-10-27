ce9cca111333654c132f608eaaf888249a60edfb
SpeekeR <DominikZappe1999@seznam.cz>
2022-10-27 17:32:18 +0200

    State machines done as maps in c++

--- server/game/GameCPP.h
+++ server/game/GameCPP.h
@@ -4,27 +4,36 @@
 #include <map>
 #include "PlayerCPP.h"
 
-constexpr unsigned char GAME_STATES_COUNT = 6;
-constexpr unsigned char GAME_EVENTS_COUNT = 5;
+constexpr unsigned char GAME_STATES_COUNT = 5;
+constexpr unsigned char GAME_EVENTS_COUNT = 6;
 
 enum GameState {
-    G_S_NOT_ALLOWED = 0,
-    G_S_INIT = 1,
-    G_S_WAITING = 2,
-    G_S_PLAYING = 3,
-    G_S_PAUSED = 4,
-    G_S_FINISHED = 5
+    G_S_WAITING,
+    G_S_PLAYING_START_OF_ROUND,
+    G_S_PLAYING_END_OF_ROUND,
+    G_S_PAUSED,
+    G_S_FINISHED
 };
 
 enum GameEvent {
-    G_E_WAIT = 0,
-    G_E_PLAY = 1,
-    G_E_PAUSE = 2,
-    G_E_FINISH = 3
+    G_E_PLAY,
+    G_E_START_ROUND,
+    G_E_END_ROUND,
+    G_E_PAUSE,
+    G_E_RECONNECT,
+    G_E_FINISH,
 };
 
 std::map<std::pair<GameState, GameEvent>, GameState> game_transitions = {
-        {{G_S_INIT, G_E_WAIT}, G_S_WAITING}
+        {{G_S_WAITING, G_E_PLAY}, G_S_PLAYING_START_OF_ROUND},
+        {{G_S_PLAYING_START_OF_ROUND, G_E_END_ROUND}, G_S_PLAYING_END_OF_ROUND},
+        {{G_S_PLAYING_START_OF_ROUND, G_E_PAUSE}, G_S_PAUSED},
+        {{G_S_PLAYING_END_OF_ROUND, G_E_START_ROUND}, G_S_PLAYING_START_OF_ROUND},
+        {{G_S_PLAYING_END_OF_ROUND, G_E_PAUSE}, G_S_PAUSED},
+        {{G_S_PLAYING_END_OF_ROUND, G_E_FINISH}, G_S_FINISHED},
+        {{G_S_PAUSED, G_E_RECONNECT}, G_S_PLAYING_START_OF_ROUND},
+        {{G_S_PAUSED, G_E_FINISH}, G_S_FINISHED},
+        {{G_S_FINISHED, G_E_PLAY}, G_S_WAITING}
 };
 
 class GameCPP{
--- server/game/PlayerCPP.h
+++ server/game/PlayerCPP.h
@@ -4,25 +4,41 @@
 #include <memory>
 #include "GameCPP.h"
 
-constexpr unsigned char PLAYER_STATES_COUNT = 6;
-constexpr unsigned char PLAYER_EVENTS_COUNT = 5;
+constexpr unsigned char PLAYER_STATES_COUNT = 5;
+constexpr unsigned char PLAYER_EVENTS_COUNT = 6;
 constexpr unsigned char PLAYER_MAX_NAME_LEN = 31;
 constexpr unsigned char NUMBER_OF_DICE = 5;
 
-enum class PlayerState {
-    P_S_NOT_ALLOWED = 0,
-    P_S_INIT = 1,
-    P_S_WAITING = 2,
-    P_S_PLAYING = 3,
-    P_S_PAUSED = 4,
-    P_S_FINISHED = 5
+enum PlayerState {
+    P_S_IN_MAIN_MENU,
+    P_S_IN_LOBBY,
+    P_S_IN_GAME_CAN_PLAY,
+    P_S_IN_GAME_CANNOT_PLAY,
+    P_S_DISCONNECTED
 };
 
-enum class PlayerEvent {
-    P_E_WAIT = 0,
-    P_E_PLAY = 1,
-    P_E_PAUSE = 2,
-    P_E_FINISH = 3
+enum PlayerEvent {
+    P_E_JOIN_LOBBY,
+    P_E_CANCEL,
+    P_E_PLAY,
+    P_E_YOUR_TURN,
+    P_E_DISCONNECT,
+    P_E_RECONNECT,
+    P_E_PLAY_AGAIN
+};
+
+std::map<std::pair<PlayerState, PlayerEvent>, PlayerState> player_transitions = {
+        {{P_S_IN_MAIN_MENU, P_E_JOIN_LOBBY}, P_S_IN_LOBBY},
+        {{P_S_IN_LOBBY, P_E_CANCEL}, P_S_IN_MAIN_MENU},
+        {{P_S_IN_LOBBY, P_E_PLAY}, P_S_IN_GAME_CAN_PLAY},
+        {{P_S_IN_GAME_CAN_PLAY, P_E_PLAY}, P_S_IN_GAME_CANNOT_PLAY},
+        {{P_S_IN_GAME_CAN_PLAY, P_E_DISCONNECT}, P_S_DISCONNECTED},
+        {{P_S_IN_GAME_CAN_PLAY, P_E_CANCEL}, P_S_IN_MAIN_MENU},
+        {{P_S_IN_GAME_CANNOT_PLAY, P_E_YOUR_TURN}, P_S_IN_GAME_CAN_PLAY},
+        {{P_S_IN_GAME_CANNOT_PLAY, P_E_DISCONNECT}, P_S_DISCONNECTED},
+        {{P_S_IN_GAME_CANNOT_PLAY, P_E_CANCEL}, P_S_IN_MAIN_MENU},
+        {{P_S_IN_GAME_CANNOT_PLAY, P_E_PLAY_AGAIN}, P_S_IN_LOBBY},
+        {{P_S_DISCONNECTED, P_E_RECONNECT}, P_S_IN_GAME_CAN_PLAY}
 };
 
 class PlayerCPP{
